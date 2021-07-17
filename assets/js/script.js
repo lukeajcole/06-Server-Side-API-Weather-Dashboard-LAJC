99fb731c552ef1e92b9dfb6359b81cd0b9894427
Luke Cole <Lukeajcole@gmail.com>
2021-07-17 15:59:04 -0700

    Working Fetch from City Strign

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,39 +1,71 @@
-{/* 
-    
-    
-<h1 class="p-2">Today's weather:</h1>
-<h3 id = "dayTemp" class="p-2">Temperature:</h3>
-<h3 id="dayWind" class="p-2">Wind Speed:</h3>
-<h3 id = "dayHumidity" class="p-2">Humidity:</h3>
-<h3 id = "dayUv" class="p-2">UV Index:</h3>
-</div> */}
-
-// <div class="card m-3 flex-shrink-0 forCard" ">
-//      <img src="..." class="card-img-top" alt="...">
-//      <div class="card-body">
-//          <h5 class="card-title">Card title</h5>
-//          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-//          <a href="#" class="btn btn-primary">Go somewhere</a>
-//      </div>
-// </div>
-
-{/* <div id='searchCont' style="background-color:antiquewhite" class="col-3 m-2 border d-flex  flex-column align-items-center">
-<h2 class="text-center m-1 p-2">Search For a City</h2>
-<input id='searchInput' class="m-2 p-2 b" type='text' placeholder="Enter a city name...">
-<button id='searchBtn' class="m-1 p-2 btn-lg">Search</button>
-</div> */}
-
 var mainCol = $('#mainCol');
 var forcastDaysEl = $('#forcastDays');
 var dayWeatherEl = $('#dayWeather');
 var searchBtn = $('#searchBtn');
 var searchInput = $('#searchInput');
+var API_key = "5117aac26351ca3a76a8a4b1d679c8eb"
+var apiData;
+var searchStr = 'san%20diego';
+
+getLocation(searchStr);
 
 
-makeDayCard();
+function getLocation(searchStr){
+    var geocodeUrl = 'http://api.openweathermap.org/geo/1.0/direct?q=' + searchStr + '&limit=5&appid=' + API_key;
+    console.log(geocodeUrl);
+        fetch(geocodeUrl).then(function(response){
+            if (response.ok) {
+                response.json().then(function(data){
+                    console.log(data);
+                    getApiResponse(data[0].lat, data[0].lon);
+                })
+            }
+        })
+    }
 
-makeForcastCards();
+function getApiResponse(lat, lon){
+    var apiUrl = 'https://api.openweathermap.org/data/2.5/onecall?lat=' + lat + '&lon=' + lon +'&units=imperial&appid=' + API_key;
+    console.log(apiUrl);
+    var weatherData;
 
+    fetch(apiUrl).then(function(response){
+        if (response.ok) {
+            response.json().then(function(data){
+                weatherData = data;
+                displayWeather(data);
+            })
+        }
+    })
+}
+
+function displayWeather(weatherData){
+    console.log(weatherData);
+    
+    makeDayCard(weatherData);
+
+    makeForcastCards();
+}
+
+function makeDayCard(weatherData){
+
+    var headEl = $("<h1 >")
+        headEl.addClass("p-2");
+        headEl.text('Today\'s Weather');
+    var tempEl = $("<h3>")
+        tempEl.addClass("p-2");
+        tempEl.text('Temperature: ' + weatherData.current.temp);
+    var windEl = $("<h3>")
+        windEl.addClass("p-2");
+        windEl.text('WindSpeed: ' + weatherData.current.wind_speed);
+    var humEl = $("<h3>")
+        humEl.addClass("p-2");
+        humEl.text('Humidity: ' + weatherData.current.humidity);
+    var uvEl = $("<h3>")
+        uvEl.addClass("p-2");
+        uvEl.text('UV Index: ' + weatherData.current.uvi);
+    
+        dayWeatherEl.append(headEl, tempEl, windEl,humEl,uvEl);
+}
 
 function makeForcastCards(){
 
@@ -41,8 +73,8 @@ function makeForcastCards(){
         var forCard = $('<div>');
             forCard.addClass('card m-3 flex-shrink-0 forCard')
         var imgEl = $('<img>');
-            imgEl.attr('src',"...");
-            imgEl.attr('alt',"...");
+            // imgEl.attr('src',"...");
+            // imgEl.attr('alt',"...");
             imgEl.addClass('card-img-top');
         var bodyEl = $('<div>');
             bodyEl.addClass('card-body');
@@ -65,24 +97,3 @@ function makeForcastCards(){
 
 
 
-
-function makeDayCard(){
-
-    var headEl = $("<h1 >")
-        headEl.addClass("p-2");
-        headEl.text('Today\'s Weather');
-    var tempEl = $("<h3>")
-        tempEl.addClass("p-2");
-        tempEl.text('Temperature');
-    var windEl = $("<h3>")
-        windEl.addClass("p-2");
-        windEl.text('WindSpeed');
-    var humEl = $("<h3>")
-        humEl.addClass("p-2");
-        humEl.text('Humidity');
-    var uvEl = $("<h3>")
-        uvEl.addClass("p-2");
-        uvEl.text('UV Index');
-    
-        dayWeatherEl.append(headEl, tempEl, windEl,humEl,uvEl);
-}
