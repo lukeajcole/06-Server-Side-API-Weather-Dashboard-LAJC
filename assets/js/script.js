136e7f1028107cb3b18f29368c058383ea3bbbab
Luke Cole <Lukeajcole@gmail.com>
2021-07-18 14:59:39 -0700

    Working Search; add unit toggle; Needs refresh from localStore

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,12 +5,71 @@ var searchBtn = $('#searchBtn');
 var searchInput = $('#searchInput');
 var API_key = "5117aac26351ca3a76a8a4b1d679c8eb"
 var apiData;
-var searchStr = 'san%20diego';
+var searchStr;
+var unitSwitch = $('#unitSwitch');
+var unitSwitchVal; //Holds the boolean of the switch state
+var unitType; //gets imperial or metric to pass into url
 
-getLocation(searchStr);
+
+// Get the last searched state of the switch or true
+unitSwitchVal = localStorage.getItem('lastUnit') || false;
+//Get the last search string or palm springs
+searchStr = localStorage.getItem('lastSearchStr') || 'palm%20springs';
+searchInput.val(decodeURI(searchStr));
+
+// Set the state of the toggle to the localStorage value and populate the variable
+// unitSwitch.prop('checked', unitSwitchVal).change();
+
+///NEED To fix this
+console.log(unitSwitchVal)
+if (unitSwitchVal == 'true'){
+    unitType = 'imperial'
+    unitSwitch.addClass('btn-primary');
+    unitSwitch.removeClass('btn-default off');
+    console.log('true switch')
+    
+} else if (unitSwitchVal == 'false')  {
+    unitType = 'metric'
+    unitSwitch.addClass('btn-default off');
+    unitSwitch.removeClass('btn-primary');
+    console.log('false switch')
+}
+
+runSearch();
+
+// update the unitType when the toggle changes
+unitSwitch.change(function() {
+    unitSwitchVal = $(this).prop('checked');
+    if (unitSwitchVal){
+        unitType = 'imperial'
+    } else {
+        unitType = 'metric'
+    }
+    localStorage.setItem('lastUnit', unitSwitchVal);
+    runSearch();
+});
 
 
-function getLocation(searchStr){
+searchBtn.on('click', function(){
+    searchStr = encodeURI(searchInput.val());
+    console.log(searchStr);
+    console.log(unitType);
+    localStorage.setItem('lastSearchStr',searchStr);
+    localStorage.setItem('lastUnit', unitSwitchVal);
+    runSearch();
+})
+
+function runSearch (){
+    // Remove Elements if needed. 
+    forcastDaysEl.empty();
+    dayWeatherEl.empty();
+    getLocation();
+
+}
+
+
+
+function getLocation(){
     var geocodeUrl = 'http://api.openweathermap.org/geo/1.0/direct?q=' + searchStr + '&limit=5&appid=' + API_key;
     console.log(geocodeUrl);
         fetch(geocodeUrl).then(function(response){
@@ -24,7 +83,7 @@ function getLocation(searchStr){
     }
 
 function getApiResponse(lat, lon){
-    var apiUrl = 'https://api.openweathermap.org/data/2.5/onecall?lat=' + lat + '&lon=' + lon +'&units=imperial&appid=' + API_key;
+    var apiUrl = 'https://api.openweathermap.org/data/2.5/onecall?lat=' + lat + '&lon=' + lon +'&units=' + unitType + '&appid=' + API_key;
     console.log(apiUrl);
     var weatherData;
 
@@ -47,19 +106,30 @@ function displayWeather(weatherData){
 }
 
 function makeDayCard(weatherData){
-
     var headEl = $("<h1 >")
         headEl.addClass("p-2");
         headEl.text('Today\'s Weather');
     var tempEl = $("<h3>")
         tempEl.addClass("p-2");
-        tempEl.text('Temperature: ' + weatherData.current.temp);
+        var tempUnit;
+        if (unitType == 'imperial'){
+            tempUnit= "F";
+        } else if (unitType == 'metric'){
+            tempUnit = "C"
+        }
+        tempEl.text('Temperature: ' + weatherData.current.temp + decodeURI('%C2%B0') + tempUnit);
     var windEl = $("<h3>")
         windEl.addClass("p-2");
-        windEl.text('WindSpeed: ' + weatherData.current.wind_speed);
+        var winUnit;
+        if (unitType == 'imperial'){
+            winUnit= "MPH";
+        } else if (unitType == 'metric'){
+            winUnit = "M/S"
+        }
+        windEl.text('WindSpeed: ' + weatherData.current.wind_speed + winUnit);
     var humEl = $("<h3>")
         humEl.addClass("p-2");
-        humEl.text('Humidity: ' + weatherData.current.humidity);
+        humEl.text('Humidity: ' + weatherData.current.humidity + "%");
     var uvEl = $("<h3>")
         uvEl.addClass("p-2");
         uvEl.text('UV Index: ' + weatherData.current.uvi);
@@ -71,7 +141,7 @@ function makeForcastCards(){
 
     for (i=0; i < 5; i++){
         var forCard = $('<div>');
-            forCard.addClass('card m-3 flex-shrink-0 forCard')
+            forCard.addClass('card m-2 flex-shrink-0 forCard')
         var imgEl = $('<img>');
             // imgEl.attr('src',"...");
             // imgEl.attr('alt',"...");
